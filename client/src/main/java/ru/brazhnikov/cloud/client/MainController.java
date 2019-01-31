73c36ed94887765d396ba8e7135a93ef51b279cb
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-01-31 14:12:04 +0300

    Удалил подключение пакета io.File в MainController.

--- client/src/main/java/ru/brazhnikov/cloud/client/MainController.java
+++ client/src/main/java/ru/brazhnikov/cloud/client/MainController.java
@@ -11,7 +11,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
